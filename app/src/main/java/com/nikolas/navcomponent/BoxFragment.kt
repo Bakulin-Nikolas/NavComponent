570199837e26255e6b3f8b21b62c87ed7057daaa
Bakulin Nikolas <nbakylin@gmail.com>
2022-04-18 09:23:14 +0300

    init and create view

---
+++ app/src/main/java/com/nikolas/navcomponent/BoxFragment.kt
@@ -0,0 +1,7 @@
+package com.nikolas.navcomponent
+
+import androidx.fragment.app.Fragment
+
+class BoxFragment : Fragment(R.layout.fragment_box) {
+
+}
---
+++ app/src/main/java/com/nikolas/navcomponent/RootFragment.kt
@@ -0,0 +1,7 @@
+package com.nikolas.navcomponent
+
+import androidx.fragment.app.Fragment
+
+class RootFragment : Fragment(R.layout.fragment_root) {
+
+}
