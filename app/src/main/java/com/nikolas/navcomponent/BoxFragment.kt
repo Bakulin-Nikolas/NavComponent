88ea18fd184de64b1271fefbe982e20f5da168fd
Bakulin Nikolas <nbakylin@gmail.com>
2022-04-20 00:25:21 +0300

    create findNavController

--- app/src/main/java/com/nikolas/navcomponent/BoxFragment.kt
+++ app/src/main/java/com/nikolas/navcomponent/BoxFragment.kt
@@ -4,4 +4,10 @@ import androidx.fragment.app.Fragment
 
 class BoxFragment : Fragment(R.layout.fragment_box) {
 
+
+
+
+    companion object {
+        const val ARG_COLOR = "color"
+    }
 }
--- app/src/main/java/com/nikolas/navcomponent/RootFragment.kt
+++ app/src/main/java/com/nikolas/navcomponent/RootFragment.kt
@@ -1,7 +1,42 @@
 package com.nikolas.navcomponent
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.nikolas.navcomponent.databinding.FragmentRootBinding
 
 class RootFragment : Fragment(R.layout.fragment_root) {
+
+    private lateinit var binding: FragmentRootBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRootBinding.bind(view)
+        binding.openYellowBoxButton.setOnClickListener {
+            openBox(Color.rgb(255, 255,200))
+        }
+        binding.openGreenBoxButton.setOnClickListener {
+            openBox(Color.rgb(200,255,200))
+        }
+    }
+
+    private fun openBox(color: Int) {
+        findNavController().navigate(
+            R.id.action_rootFragment_to_boxFragment,
+            bundleOf(BoxFragment.ARG_COLOR to color),
+            navOptions {
+                anim {
+                    enter = R.anim.enter
+                    exit = R.anim.exit
+                    popEnter = R.anim.pop_enter
+                    popExit = R.anim.pop_exit
+                }
+            }
+        )
+    }
 
 }
