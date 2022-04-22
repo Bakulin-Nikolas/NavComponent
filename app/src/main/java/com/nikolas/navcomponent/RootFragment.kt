bbfea865a85ad436e5cc8b72bfb721e5faf9bd06
Bakulin Nikolas <nbakylin@gmail.com>
2022-04-23 00:12:34 +0300

    Safe Args data exchange

--- app/src/main/java/com/nikolas/navcomponent/RootFragment.kt
+++ app/src/main/java/com/nikolas/navcomponent/RootFragment.kt
@@ -18,22 +18,33 @@ class RootFragment : Fragment(R.layout.fragment_root) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRootBinding.bind(view)
         binding.openYellowBoxButton.setOnClickListener {
-            openBox(Color.rgb(255, 255,200), R.string.color_name_yellow.toString())
+            openBox(Color.rgb(255, 255,200), getString(R.string.color_name_yellow))
         }
         binding.openGreenBoxButton.setOnClickListener {
-            openBox(Color.rgb(200,255,200), R.string.color_name_green.toString())
+            openBox(Color.rgb(200,255,200), getString(R.string.color_name_green))
         }
 
-        parentFragmentManager.setFragmentResultListener(BoxFragment.REQUEST_CODE, viewLifecycleOwner) { requestCode, data ->
-            val number = data.getInt(BoxFragment.EXTRA_RANDOM_NUMBER)
-            Toast.makeText(requireContext(), "Generate number : $number", Toast.LENGTH_SHORT).show()
+        val livedata = findNavController().currentBackStackEntry
+            ?.savedStateHandle?.getLiveData<Int>(BoxFragment.EXTRA_RANDOM_NUMBER)
+        livedata?.observe(viewLifecycleOwner) { randomNumber ->
+            if(randomNumber != null) {
+                Toast.makeText(requireContext(), "Generate number : $randomNumber", Toast.LENGTH_SHORT).show()
+            }
+            livedata.value = null
         }
+//        parentFragmentManager.setFragmentResultListener(BoxFragment.REQUEST_CODE, viewLifecycleOwner) { requestCode, data ->
+//            val number = data.getInt(BoxFragment.EXTRA_RANDOM_NUMBER)
+//            Toast.makeText(requireContext(), "Generate number : $number", Toast.LENGTH_SHORT).show()
+//        }
     }
 
     private fun openBox(color: Int, colorName: String) {
+
+        val direction = RootFragmentDirections.actionRootFragmentToBoxFragment(color, colorName)
         findNavController().navigate(
-            R.id.action_rootFragment_to_boxFragment,
-            bundleOf(BoxFragment.ARG_COLOR to color, BoxFragment.ARG_COLOR_NAME to colorName),
+            direction,
+//            R.id.action_rootFragment_to_boxFragment,
+//            bundleOf(BoxFragment.ARG_COLOR to color, BoxFragment.ARG_COLOR_NAME to colorName),
             navOptions {
                 anim {
                     enter = R.anim.enter
